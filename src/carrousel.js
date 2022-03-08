ce81e45b552ebfb337dc011922527b9cfe4a0a4c
tomivm <tomivm98@gmail.com>
2022-03-08 10:27:46 -0300

    Carrousel and things

---
+++ src/carrousel.js
@@ -0,0 +1,35 @@
+let slidePosition = 1;
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow((slidePosition += n));
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow((slidePosition = n));
+}
+
+function SlideShow(n) {
+  let i;
+  let slides = document.getElementsByClassName("Containers");
+  let circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {
+    slidePosition = 1;
+  }
+  if (n < 1) {
+    slidePosition = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+    circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition - 1].style.display = "block";
+  circles[slidePosition - 1].className += " enable";
+}
+window.onload = init;
+function init() {
+  SlideShow(slidePosition);
+}
