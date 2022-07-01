dd19b09e1475588da320c372fd4664b0956d74e0
lmbringas <lm.bringas98@gmail.com>
2022-07-01 08:01:20 -0300

    ✨ change about

--- faatra/home/urls.py
+++ faatra/home/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("about", views.who_we_are, name="who-we-are"),
+    path("quienes-somos", views.who_we_are, name="who-we-are"),
     path("", views.index, name="index"),
 ]
