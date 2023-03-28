605f2a77dc0e55f9e5242e58a6e9926f51e574cb
lmbringas <lm.bringas98@gmail.com>
2023-03-28 17:04:02 -0300

    ⚡️

--- faatra/incriptions/admin.py
+++ faatra/incriptions/admin.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.contrib import admin
 
 from import_export.admin import ExportActionMixin
+from import_export.resources import ModelResource
 from .models import Incription, EmployeeCondition
 
 # Register your models here.
@@ -14,9 +15,44 @@ from .models import Incription, EmployeeCondition
 class EmployeeConditionAdmin(admin.ModelAdmin):
     pass
 
+class InscriptionResource(ModelResource):
+    def get_export_headers(self):
+        headers = super().get_export_headers()
+        for i, h in enumerate(headers):
+            h = h.lower()
+            if h == 'created date':
+                headers[i] = "Fecha de creacion"
+            if h == 'city':
+                headers[i] = "Ciudad"
+            if h == 'county':
+                headers[i] = "Provincia"
+            if h == 'birth_date':
+                headers[i] = "Fecha de nacimiento"
+            if h == 'address':
+                headers[i] = "Direccion"
+            if h == 'fullname':
+                headers[i] = "Nombre completo"
+            if h == 'zip_code':
+                headers[i] = "Codigo postal"
+            if h == 'employee_condition':
+                headers[i] = "Nombre completo"
+            if h == 'course':
+                headers[i] = "Curso"
+            if h == 'is_confirmed':
+                headers[i] = "Confirmado"
+            if h == 'comment':
+                headers[i] = "Comentario"
+            if h == 'employee_condition':
+                headers[i] = "Condicion laboral"
+        return headers
+
+    class Meta:
+        model = Incription
 
 @admin.register(Incription)
 class IncriptionAdmin(ExportActionMixin, admin.ModelAdmin):
+
+    resource_classes = [InscriptionResource]
     list_display = ("fullname", "email", "created_date", "course", "is_confirmed")
     list_editable = ("is_confirmed",)
     fields = (
