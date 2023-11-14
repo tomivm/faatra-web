d1209ada1e5f62eb5e6d745b5bf9f4f6d658da13
tomivm <tomivm98@gmail.com>
2023-11-13 22:04:26 -0300

    Improve focusing and styles of services gallery

--- faatra/static/js/services.js
+++ faatra/static/js/services.js
@@ -62,15 +62,17 @@ prepareArrowsForScroll = () => {
   const buttonRight = document.getElementById("slideRight");
   const buttonLeft = document.getElementById("slideLeft");
   const galleryContainer = document.getElementById("gallery");
-  const liGalleryItems = document.getElementsByClassName("gallery-item")
+  const liGalleryItems = document.getElementsByClassName("gallery-item");
+  const serviceButtons = document.getElementsByClassName("services-buttom");
+
+
+  const serviceButtonsArray = Array.prototype.map.call(serviceButtons, item => item);
 
   const galleryItemDefaultPadding = window
     .getComputedStyle(buttonRight, null)
     .getPropertyValue("width");
 
-  const arrowsWidth =parseInt(galleryItemDefaultPadding, 10) * 2;
-
-  
+  const arrowsWidth =parseFloat(galleryItemDefaultPadding, 10) + 5;
   const galleryItemDefaultWidth = galleryContainer.clientWidth - arrowsWidth;
 
   const isScrolleable =
@@ -80,30 +82,45 @@ prepareArrowsForScroll = () => {
 
   if(isScrolleable){
     Array.prototype.map.call(liGalleryItems, (item)=>{item.style.width=`${galleryItemDefaultWidth}px`});
+    buttonLeft.disabled = true;
   }
 
   if (!isScrolleable) {
     buttonRight.classList.add("disabled");
+    buttonRight.disabled = true;
     buttonLeft.classList.add("disabled");
+    buttonLeft.disabled = true;
   }
 
+  let itemFocused = 0
+
   buttonRight.onclick = function () {
+    itemFocused += 1
     const scrollLeft = (galleryContainer.scrollLeft += galleryItemDefaultWidth);
     buttonLeft.classList.remove("disabled");
+    buttonLeft.disabled = false;
 
     const scrollRight =
       galleryContainer.scrollWidth - galleryContainer.clientWidth;
     if (scrollLeft >= scrollRight) {
       buttonRight.classList.add("disabled");
+      buttonRight.disabled = true;
     }
+    serviceButtonsArray[itemFocused].onclick();
+    serviceButtonsArray[itemFocused].focus();
   };
 
   buttonLeft.onclick = function () {
+    itemFocused -= 1
     const scrollRemaining = (galleryContainer.scrollLeft -=
       galleryItemDefaultWidth);
     buttonRight.classList.remove("disabled");
+    buttonRight.disabled = false;
     if (scrollRemaining <= 0) {
       buttonLeft.classList.add("disabled");
+      buttonLeft.disabled = true;
     }
+    serviceButtonsArray[itemFocused].onclick();
+    serviceButtonsArray[itemFocused].focus();
   };
 };
