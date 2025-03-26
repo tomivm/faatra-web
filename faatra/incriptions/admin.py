f51a08ed6f7dd707462c5d6ca377e9f4e58f2e46
lmbringas <lm.bringas98@gmail.com>
2025-03-26 00:24:56 -0300

    🐛

--- faatra/incriptions/admin.py
+++ faatra/incriptions/admin.py
@@ -8,7 +8,7 @@ from import_export.admin import ExportActionModelAdmin
 from import_export import fields
 from import_export.resources import ModelResource
 from .models import Incription, EmployeeCondition
-
+from django.db.models import Q
 # Register your models here.
 
 
@@ -32,7 +32,22 @@ class InscriptionResource(ModelResource):
         model = Incription
         fields = ("fullname", "email", "telefono", "dni", "birth_date", "address", "zip_code", "city", "county", "comment", "is_confirmed", "created_date", "employee_condition__name", "course__title")
 
-#@admin.register(Incription)
+class SaloonTitleFilter(admin.SimpleListFilter):
+    title = "Cámara"  # Nombre del filtro en el admin
+    parameter_name = "saloon_title"  # Nombre del parámetro en la URL
+
+    def lookups(self, request, model_admin):
+        """Devuelve las opciones del filtro (cámaras disponibles)"""
+        saloons = set(model_admin.model.objects.values_list("course__saloon__title", flat=True))
+        return [(s, s) for s in saloons if s]  # Filtra los valores no vacíos
+
+    def queryset(self, request, queryset):
+        """Aplica el filtro cuando se selecciona un valor"""
+        if self.value():
+            return queryset.filter(Q(course__saloon__title=self.value()))
+        return queryset 
+
+@admin.register(Incription)
 class IncriptionAdmin(ExportActionModelAdmin):
     resource_class = InscriptionResource
     list_display = ("fullname", "email", "created_date", "course", "is_confirmed", "get_saloon")
@@ -61,6 +76,5 @@ class IncriptionAdmin(ExportActionModelAdmin):
 
     readonly_fields = ["created_date"]
     search_fields = ("fullname", "email", "dni")
-    list_filter = ("is_confirmed", "created_date", "course", "course__saloon__title")
+    list_filter = ("is_confirmed", "created_date", "course", SaloonTitleFilter, )
 
-admin.site.register(Incription, IncriptionAdmin)
